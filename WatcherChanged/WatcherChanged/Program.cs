79dc97cef6ab08a7ed460877b037d14ee80047cc
alser <nagmetova.adel@gmail.com>
2016-01-20 23:21:16 +0600

    added watcher

---
+++ WatcherChanged/WatcherChanged/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatcherChanged
+{
+    class Program
+    {
+        static void watcher_changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Directory changed{0}, {1}", e.GetType(), e.FullPath);
+
+        }
+        static void watcher_changed1(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Directory changed {0},{1}", e.GetType(), e.FullPath);
+        }
+        static void watcher_changed2(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Deleted {0}, {1}", e.GetType(), e.FullPath);
+        }
+
+        static void Main(string[] args)
+        {
+            FileSystemWatcher fw = new FileSystemWatcher();
+            fw.Path = @"c:\Users\alser\Desktop\Adel";
+            fw.Created += new FileSystemEventHandler(watcher_changed);
+            fw.Created += new FileSystemEventHandler(watcher_changed1);
+
+            fw.Deleted += new FileSystemEventHandler(watcher_changed2);
+            fw.EnableRaisingEvents = true;
+            Console.ReadKey();
+
+         
+        }
+    }
+}
