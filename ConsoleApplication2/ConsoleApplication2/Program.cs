ce394e277a6f7e621dafb8579b82216f71635c2a
alser <nagmetova.adel@gmail.com>
2016-01-24 12:14:25 +0600

    Lab1 prime numbers

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+            foreach (string s in numbers)
+            {
+                int a = int.Parse(s);
+                bool p = false;
+                for ( int i = 2; i*i <= a; i++)
+                {
+                    if (a % i == 0)
+                        p = true;
+                }
+                if (p == false)
+                    Console.Write(a);  
+            }
+            Console.ReadKey();
+        }
+    }
+}
