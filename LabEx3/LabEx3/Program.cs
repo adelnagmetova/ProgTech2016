d805f687871a0617a86125fcfa77ca7b72f216da
alser <nagmetova.adel@gmail.com>
2016-01-31 22:07:06 +0600

    Lab2 piazza additional task

---
+++ LabEx3/LabEx3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabEx3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"c:\Proga");
+
+            int index = 0;
+            FileInfo[] files = d.GetFiles();
+            bool showFileData = false;
+            while (true)
+            {
+                if (showFileData == false)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        if (index == i)
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        Console.WriteLine(files[i].Name);
+                    }
+                }
+                ConsoleKeyInfo button = Console.ReadKey();
+                if (button.Key == ConsoleKey.UpArrow)
+                {
+                    if (index > 0)
+                        index--;
+                }
+                if (button.Key == ConsoleKey.DownArrow)
+                {
+                    if (index < files.Length - 1)
+                        index++;
+                }
+                if (button.Key == ConsoleKey.Enter)
+                {
+                    FileStream f = new FileStream(files[index].FullName, FileMode.OpenOrCreate, FileAccess.Read);
+                    StreamReader sr = new StreamReader(f);
+                    Console.Write(sr.ReadToEnd());
+                    sr.Close();
+                    f.Close();
+                    showFileData = true;
+
+                }
+                if (button.Key == ConsoleKey.Escape)
+                    showFileData = false;
+
+                if (showFileData == false)
+                    Console.Clear();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabEx3/LabEx3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("LabEx3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LabEx3")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("137a23bd-6a49-4e6e-b07d-7805849ea50c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
