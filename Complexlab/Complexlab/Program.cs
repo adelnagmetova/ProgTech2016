c9a093cea7b013fd627cded7180908b84cdff5f0
alser <nagmetova.adel@gmail.com>
2016-02-07 11:32:48 +0600

    operator -

--- Complexlab/Complexlab/Program.cs
+++ Complexlab/Complexlab/Program.cs
@@ -20,7 +20,7 @@ namespace Complexlab
             return a + "/" + b;
           
         }
-        public static Complex operator +(Complex c1, Complex c2)
+        /*public static Complex operator +(Complex c1, Complex c2)
         {
             int numerator = c1.a * c2.b + c2.a * c1.b;
             int denominator = c1.b * c2.b;
@@ -29,8 +29,17 @@ namespace Complexlab
             denominator /= g;
             return new Complex(numerator, denominator); 
 
+        }*/
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            int numerator = c1.a * c2.b - c2.a * c1.b;
+            int denominator = c1.b * c2.b;
+            int g = gcd(numerator, denominator);
+            numerator /= g;
+            denominator /= g;
+            return new Complex(numerator, denominator);
         }
-        private static int gcd(int x, int y)
+        public  static int gcd(int x, int y)
         {
             if (y == 0)
                 return x;
@@ -46,7 +55,7 @@ namespace Complexlab
             up = int.Parse(token[0]);
             down = int.Parse(token[1]);
             Complex c2 = new Complex(up, down);
-            Complex c3 = c1 + c2;
+            Complex c3 = c1 - c2;
             Console.WriteLine(c3);
             Console.ReadKey();
 
